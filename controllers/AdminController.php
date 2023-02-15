ffe0ae3442e8ecca233f855617a6291f4148cb02
Elisa Haxhillazi <elisahaxhillazi@gmail.com>
2023-02-16 00:38:49 +0100

    delete user

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -6,15 +6,20 @@ define("SECRET_KEY", "test123");
 
 use Error;
 use Models\UsersModel;
-
+use Models\PurchasesModel;
 
 class AdminController extends BaseController
 {
     private $requestMethod;
 
+    private $purchasesModel;
+    private $userModel;
+
     public function __construct()
     {
         parent::__construct();
+        $this->purchasesModel = new PurchasesModel;
+        $this->userModel = new UsersModel;
     }
 
     public function getAllAction()
@@ -30,6 +35,45 @@ class AdminController extends BaseController
                     $arrUsers = $userModel->getAll();
 
                     $responseData = $arrUsers;
+                } catch (Error $e) {
+                    $strErrorDesc = $e->getMessage() . 'Something went wrong! Please contact support.';
+                    $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+                }
+            } else {
+                $strErrorDesc = 'Method not supported';
+                $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+            }
+        } else {
+            $strErrorDesc = 'User not authorized';
+            $strErrorHeader = 'HTTP/1.1 402 Not Authorized';
+        }
+
+        // send output
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(
+                json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
+
+    function deleteAction()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+        if ($this->validateToken('ADMIN')) {
+            if (strtoupper($requestMethod) == 'POST') {
+                try {
+                    $data = (array) json_decode(file_get_contents('php://input'), TRUE);
+                    $result = $this->purchaseModel->delete(["Purchase" => $data["Id"]]);//product with that id
+                    $result = $this->UsersModel->delete($data);//user with that id
+                    $responseData = $result;
                 } catch (Error $e) {
                     $strErrorDesc = $e->getMessage() . 'Something went wrong! Please contact support.';
                     $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
