9f3143b3f4a958b755c68de249c77be204bc88a1
smartiri <rasa7825@gmail.com>
2023-02-16 00:05:27 +0100

    delete purchases

--- controllers/PurchaseController.php
+++ controllers/PurchaseController.php
@@ -140,7 +140,42 @@ class PurchaseController extends BaseController
             );
         }
     }
-    // public function deleteAction(){
+    public function deleteAction()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-    // }
+        if ($this->validateToken('ADMIN')) {
+            if (strtoupper($requestMethod) == 'POST') {
+                try {
+                    $data = (array) json_decode(file_get_contents('php://input'), TRUE);
+                    $result = $this->purchasedItemsModel->delete(["Purchase" => $data["Id"]]);
+                    $result = $this->purchasesModel->delete($data);
+                    $responseData = $result;
+                } catch (Error $e) {
+                    $strErrorDesc = $e->getMessage() . 'Something went wrong! Please contact support.';
+                    $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+                }
+            } else {
+                $strErrorDesc = 'Method not supported';
+                $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+            }
+        } else {
+            $strErrorDesc = 'User not authorized';
+            $strErrorHeader = 'HTTP/1.1 402 Not Authorized';
+        }
+
+        // send output
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(
+                json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
 }
